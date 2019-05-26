af2bdc4287468ecc7aa84b4de9b7c3ba85b23248
Ted Kim <yenafirst91@gmail.com>
2019-05-26 02:15:00 -0400

    Include posts, followings, and followers

--- routes/user.js
+++ routes/user.js
@@ -49,13 +49,32 @@ router.post('/login', (req, res, next) => {
       return res.status(401).json(info);
       // return res.status(401).send(info.reason);
     }
-    return req.login(user, (loginErr) => {
-      if (loginErr) {
-        return next(loginErr);
+    return req.login(user, async (loginErr) => {
+      try {
+        if (loginErr) {
+          return next(loginErr);
+        }
+        const fullUser = await db.User.findOne({
+          where: { id: user.id },
+          include: [{
+            model: db.Post,
+            as: 'Posts',
+            attributes: ['id'],
+          }, {
+            model: db.User,
+            as: 'Followings',
+            attributes: ['id'],
+          }, {
+            model: db.User,
+            as: 'Followers',
+            attributes: ['id'],
+          }],
+          attributes: ['id', 'nickname', 'userId'],
+        });
+        return res.json(fullUser);
+      } catch(e) {
+        next(e);
       }
-      const filteredUser = Object.assign({}, user.toJSON());
-      delete filteredUser.password;
-      return res.json(filteredUser);
     });
   })(req, res, next);
 });
