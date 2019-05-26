9c8b97fb1090fb644cc400b7f8e66809dc1d2577
Ted Kim <dev.ted.kim@gmail.com>
2019-05-25 23:33:12 -0400

    dotenv, cookie, session

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const dotenv = require('dotenv');
+
 
 const db = require('./models');
 const userAPIRouter = require('./routes/user');
 const postAPIRouter = require('./routes/post');
 const postsAPIRouter = require('./routes/posts');
 
+dotenv.config();
 const app = express();
 db.sequelize.sync();
 
@@ -15,6 +20,16 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(expressSession({
+  resave: true,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,  // https
+  }
+}));
 
 app.use('/api/user', userAPIRouter);
 app.use('/api/post', postAPIRouter);
